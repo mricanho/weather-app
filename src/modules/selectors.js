8ac71c16763c5963b84e44c5da5d1ee6a1097237
Miguel Ricaño <mricanho@gmail.com>
2021-04-05 14:09:44 -0500

    Create selectors.js

---
+++ src/modules/selectors.js
@@ -0,0 +1,7 @@
+export const locationForm = document.querySelectorAll('.needs-validation');
+export const findButton = document.querySelectorAll('button[id^="findBtn"]');
+export const locationInput = document.querySelector('#locationInpt');
+export const mainWeatherSection = document.querySelector('.mainSection');
+export const loadingPageAnimation = document.querySelector('#displayWhileLoadingPage');
+export const celsiusBtn = document.querySelector('#celsius');
+export const fahrenheitBtn = document.querySelector('#fahrenheit');
