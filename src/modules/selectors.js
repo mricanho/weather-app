32d1f55ae06512837441c5a6bdbf1ff37f5b3e5b
Miguel Ricaño <mricanho@gmail.com>
2021-04-06 11:29:13 -0500

    Add all selectors

--- src/modules/selectors.js
+++ src/modules/selectors.js
@@ -5,3 +5,14 @@ export const mainWeatherSection = document.querySelector('.mainSection');
 export const loadingPageAnimation = document.querySelector('#displayWhileLoadingPage');
 export const celsiusBtn = document.querySelector('#celsius');
 export const fahrenheitBtn = document.querySelector('#fahrenheit');
+
+export const todayTemp = document.querySelector('#temp');
+export const todayCard = document.querySelector('#day-0');
+export const todayCondition = document.querySelector('#day-0 #condition');
+export const todayConditionIcon = document.querySelector('#day-0 #temp-icon');
+export const todayHumidity = document.querySelector('#day-0 #humidity');
+export const todayWindDirection = document.querySelector('#day-0 #windDirection');
+export const todayCityName = document.querySelector('#city');
+export const todayWindSpeed = document.querySelector('#day-0 #windSpeed');
+export const todayName = document.querySelector('#day-0 #day');
+export const todayDate = document.querySelector('#day-0 #date');
