5da988a18fda2eb0c8c67f9610d2b1fee232b30a
Miguel Ricaño <mricanho@gmail.com>
2021-04-08 13:24:03 -0500

    Update webpack.config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
         test: /\.(jpe?g|png|gif)$/,
         loader: 'url-loader',
         options: {
-          // Images larger than 10 KB won’t be inlined
           limit: 10 * 1024,
           name: 'img/[hash].[ext]',
         },
@@ -26,23 +25,17 @@ module.exports = {
         test: /\.svg$/,
         loader: 'svg-url-loader',
         options: {
-          // Images larger than 10 KB won’t be inlined
           limit: 10 * 1024,
-          // Remove quotes around the encoded URL –
-          // they’re rarely useful
           noquotes: true,
         },
       },
       {
         test: /\.(jpg|png|gif|svg)$/,
         loader: 'image-webpack-loader',
-        // Specify enforce: 'pre' to apply the loader
-        // before url-loader/svg-url-loader
-        // and not duplicate it in rules with them
         enforce: 'pre',
         options: {
-          bypassOnDebug: true, // webpack@1.x
-          disable: true, // webpack@2.x and newer
+          bypassOnDebug: true, 
+          disable: true, 
         },
       },
     ],
